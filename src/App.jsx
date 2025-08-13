295875cb2bcb1f0e994d418817259965261a35f7
root <root@BESTHOR-IGBE.localdomain>
2025-08-13 15:02:39 +0100

    adding more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection";
+import CustomerCursor from "./components/CustomerCursor";
 
 export default function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <CustomerCursor />
     </>
   )
 }
